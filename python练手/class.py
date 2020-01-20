0fff5ff95d672cde176fa07215643637aba15e87
Zhang YongLiang <dream20151224@163.com>
2020-01-20 17:29:38 +0800

    python class

--- python练手/class.py
+++ python练手/class.py
@@ -8,7 +8,9 @@ class Scene(object):
         exit(1)
 
 class Apple(Scene):
-    pass
+    def enter(self):
+        print("This is apple")
+        return 'death'
 
 class Banana(Scene):
     pass
@@ -18,14 +20,41 @@ class Orange(Scene):
 
 class Peal(Scene):
     pass
+
 class Death(Scene):
-    pass
+    def enter(self):
+        print("You do a great job\n")
+        exit(1)
+class Finish(Scene):
+    def enter(self):
+        print("This is finish\n")
+        return 'finish'
+
 class Map(object):
     my_map={'apple' : Apple(),
            'banana' : Banana(),
            'orange' : Orange(),
             'peal' : Peal(),
-            'finish' : Death()}
+            'death': Death(),
+            'finish' : Finish()}
+    def __init__(self,start_scene):
+        self.start_scene = start_scene
+    def next_scene(self,scene_name):
+        val = Map.my_map.get(scene_name)
+        return val
+    def open_scene(self):
+        return self.next_scene(self.start_scene)
+
 class Engine(object):
     def __init__(self,map):
-        self.map=map
+        self.map = map
+    def play(self):
+        open_scene = self.map.open_scene()
+        last_scene = self.map.next_scene('finish')
+        while(open_scene != last_scene):
+         next_scene_name = open_scene.enter()
+         open_scene = self.map.next_scene(next_scene_name)
+
+a_map = Map('apple')
+engine = Engine(a_map)
+engine.play()
